d293f19dc0324872f3971bb8a5290e423f0e002f
Zap <67962871+proZap@users.noreply.github.com>
2020-10-18 19:58:45 +0700

    Minor fixes

--- js/application.js
+++ js/application.js
@@ -1,10 +1,8 @@
-// Wait till the browser is ready to render the game (avoids glitches)                  //
-var game;                                                                               //
-window.requestAnimationFrame(function () {                                              //
-    game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager); //
-});                                                                                     //
-                                                                                        //
-//////////////////////////////////////////////////////////////////////////////////////////
+// Wait till the browser is ready to render the game (avoids glitches)
+var game;
+window.requestAnimationFrame(function () {
+    game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
 
 var aiRunning = false;
 
@@ -16,43 +14,23 @@ var workers = [
 ];
 
 var working = 0;
-var bestResult = 0;
-var currentDepth = 3;
-var bestMove = 0;
-var stateEvaled = 0;
-var lastStateEvaled = 0;
-var minStateEval = 40000;
-
-//var startTime, moves;
+var bestMove, bestResult;
 
 for (let i = 0; i < 4; i++) {
     workers[i].onmessage = function(e) {
         working--;
-        stateEvaled += e.data.stateEvaled;
-        if (e.data.result > bestResult) {
-            bestResult = e.data.result;
+        if (e.data > bestResult) {
+            bestResult = e.data;
             bestMove = i;
         }
         if (working == 0) {
-            if (stateEvaled >= minStateEval || stateEvaled <= lastStateEvaled) {
-                if (bestResult > 0) game.move(bestMove);
-                else game.move(0 | 4 * Math.random());
-                if (game.over) stopAI();
-                if (game.won) {
-                    game.keepPlaying = true;
-                    game.actuator.clearMessage();
-                }
-                currentDepth = 3;
-                lastStateEvaled = 0;
-                minStateEval = 40000;
-                if (aiRunning) step();
+            game.move(bestMove);
+            if (game.over) stopAI();
+            if (game.won) {
+                game.keepPlaying = true;
+                game.actuator.clearMessage();
             }
-            else {
-                currentDepth++;
-                lastStateEvaled = stateEvaled;
-                minStateEval *= 2;
-                step();
-            }
+            if (aiRunning) step();
         }
     }
 }
@@ -69,23 +47,20 @@ function currentState() {
 }
 
 function step() {
-    working = 4;
-    stateEvaled = 0;
+    var board = currentState();
     bestResult = 0;
+    working = 4;
+    bestMove = 0 | 4 * Math.random();
     for (var i = 0; i < 4; i++) {
-        var board = currentState();
         workers[i].postMessage({
             board: board,
             dir: i,
-            depth: currentDepth
         })
     }
 }
 
 function initAI() {
     startAI = () => {
-        //startTime = Date.now();
-        //moves = 0;
         document.getElementsByClassName("ai-buttons")[1].innerHTML = "Stop";
         aiRunning = true;
         step();
@@ -95,8 +70,6 @@ function initAI() {
 
 
 function stopAI() {
-    //var secs = (Date.now() - startTime) / 1000;
-    //console.log("Total time: " + Math.floor(secs / 60) + " minutes " + Math.floor(secs) % 60 + " seconds\nTotal moves: "+ moves + "\nSpeed: " + moves / secs + " moves per second");
     document.getElementsByClassName("ai-buttons")[1].innerHTML = "Start AI";
     aiRunning = false;
     initAI();
